ad0525ca7361ff1997d5c33e638969ff1d0b6893
lisyuan <lisyuan0429@gmail.com>
2020-07-24 20:55:09 +0800

    rename

--- model/build_dataset.py
+++ model/build_dataset.py
@@ -1,37 +1,41 @@
 import numpy as np
 import os
 
-from torch.utils.data.dataset import Dataset
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torch.utils.data import DataLoader
 from PIL import Image
 
 INPUT_DIR = '../data'
 
-
-
 class VQADataset(Dataset):
 
-    def __init__(self, input_dir, input_file, max_qu_len = 30, max_ans_len = 10):
+    def __init__(self, input_dir, input_file, max_qu_len = 30, transform = None):
 
         self.input_data = np.load(os.path.join(input_dir, input_file), allow_pickle=True)
         self.qu_vocab = Vocab(input_dir+'/question_vocabs.txt')
         self.ans_vocab = Vocab(input_dir+'/annotation_vocabs.txt')
         self.max_qu_len = max_qu_len
-        self.max_ans_len = max_ans_len
         self.labeled = True if not "test" in input_file else False
+        self.transform = transform
 
     def __getitem__(self, idx):
 
         path = self.input_data[idx]['img_path']
-        img = np.array(Image.open(path))
+        img = np.array(Image.open(path).convert('RGB'))
+        qu_id = self.input_data[idx]['qu_id']
         qu_tokens = self.input_data[idx]['qu_tokens']
         qu2idx = np.array([self.qu_vocab.word2idx('<pad>')] * self.max_qu_len)
         qu2idx[:len(qu_tokens)] = [self.qu_vocab.word2idx(token) for token in qu_tokens]
-        sample = {'image': img, 'question': qu2idx}
+        sample = {'image': img, 'question': qu2idx, 'question_id': qu_id}
 
         if self.labeled:
             ans2idx = [self.ans_vocab.word2idx(ans) for ans in self.input_data[idx]['valid_ans']]
             ans2idx = np.random.choice(ans2idx)
             sample['answer'] = ans2idx
+
+        if self.transform:
+            sample['image'] = self.transform(sample['image'])
 
         return sample
 
@@ -39,6 +43,36 @@ class VQADataset(Dataset):
 
         return len(self.input_data)
 
+def data_loader(input_dir, batch_size, max_qu_len, num_worker):
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),  # convert to (C,H,W) and [0,1]
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))  # mean=0; std=1
+    ])
+
+    vqa_dataset = {
+        'train': VQADataset(
+            input_dir=input_dir,
+            input_file='train.npy',
+            max_qu_len=max_qu_len,
+            transform=transform),
+        'val': VQADataset(
+            input_dir=input_dir,
+            input_file='val.npy',
+            max_qu_len=max_qu_len,
+            transform=transform)
+    }
+
+    dataloader = {
+        key: DataLoader(
+            dataset=vqa_dataset[key],
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_worker)
+        for key in ['train', 'val']
+    }
+
+    return dataloader
 
 class Vocab:
 
@@ -46,7 +80,7 @@ class Vocab:
 
         self.vocab = self.load_vocab(vocab_file)
         self.vocab2idx = {vocab: idx for idx, vocab in enumerate(self.vocab)}
-
+        self.vocab_size = len(self.vocab)
 
     def load_vocab(self, vocab_file):
 
@@ -62,12 +96,6 @@ class Vocab:
         else:
             return self.vocab2idx['<unk>']
 
-if __name__ == "__main__":
+    def idx2word(self, idx):
 
-    torchdata = VQADataset(INPUT_DIR, 'train.npy')
-    print(torchdata)
-
-    qu_vocab = Vocab(INPUT_DIR+'/question_vocabs.txt')
-    print(qu_vocab)
-    for i in range(3):
-        print(torchdata[i])
+        return self.vocab[idx]
