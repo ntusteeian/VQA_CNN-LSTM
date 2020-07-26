4ae7bcc51de807ff84bed273081eb2af76ff0682
lisyuan <lisyuan0429@gmail.com>
2020-07-27 03:54:06 +0800

    tune parameters

--- model/train.py
+++ model/train.py
@@ -12,11 +12,11 @@ CKPT_DIR = '../ckpt'
 LOG_DIR = '../log'
 
 BATCH_SIZE = 150
-MAX_QU_LEN = 28
+MAX_QU_LEN = 30
 NUM_WORKER = 8
 FEATURE_SIZE, WORD_EMBED = 1024, 300
 NUM_HIDDEN, HIDDEN_SIZE = 2, 512
-LEARNING_RATE, STEP_SIZE, GAMMA = 0.001, 5, 0.1
+LEARNING_RATE, STEP_SIZE, GAMMA = 0.001, 10, 0.1
 EPOCH = 50
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
